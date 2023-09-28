f0b396a7b9d3d10e292ee3dd9188b965a024ba96
Sakshi-Git1 <agrasaks@in.ibm.com>
2023-09-28 14:16:47 +0000

    aifile

--- demo1/src/main/java/com/example/demo/Demo1Application.java
+++ demo1/src/main/java/com/example/demo/Demo1Application.java
@@ -1,17 +1,8 @@
-package com.example.demo;
 import io.javalin.Javalin;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class Demo1Application {
-    public static void main(String[] args) {
-        Javalin app = Javalin.create();
-
-        app.get("/", ctx -> ctx.result("Hello World from Java!"));
-
-        app.start(8080);
-    }
-
+public class Main {
+ public static void main(String[] args) {
+ Javalin app = Javalin.create().start(8080);
+ app.get("/", ctx -> ctx.result("Hello World from Java!"));
+ }
 }
