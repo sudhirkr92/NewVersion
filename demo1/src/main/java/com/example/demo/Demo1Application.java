cf8ba2115962c24f8e3da2977dbb379aa45e748d
Sakshi-Git1 <agrasaks@in.ibm.com>
2023-09-28 15:17:03 +0000

    aigeneratedfile

--- demo1/src/main/java/com/example/demo/Demo1Application.java
+++ demo1/src/main/java/com/example/demo/Demo1Application.java
@@ -1,11 +1,10 @@
 import io.javalin.Javalin;
 
 public class Main {
+
  public static void main(String[] args) {
  Javalin app = Javalin.create().start(8080);
  
- app.get("/", ctx -> {
- ctx.result("Hello World from Java!");
- });
+ app.get("/", ctx -> ctx.result("Hello World from Java!"));
  }
 }
