157ba349f27066eb0ac74b2c454360fe6bd1c0da
Sakshi-Git1 <agrasaks@in.ibm.com>
2023-09-29 22:45:17 +0530

    new checkin

---
+++ demo1/src/main/java/com/example/demo/main.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+
+import org.springframework.stereotype.Service;
+
+import io.javalin.Javalin;
+
+@Service
+public class main {
+    public static void main(String[] args) {
+        Javalin app = Javalin.create().start(8080);
+        
+        app.get("/", ctx -> {
+        ctx.result("Hello World from Java!");
+        });
+        }
+}
+
