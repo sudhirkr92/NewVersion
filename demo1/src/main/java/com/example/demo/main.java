34321100cf78c82edc0382f82fe720bb8beee640
sudhirkr92 <109576128+sudhirkr92@users.noreply.github.com>
2023-10-01 14:46:38 +0100

    Update main.java

--- demo1/src/main/java/com/example/demo/main.java
+++ demo1/src/main/java/com/example/demo/main.java
@@ -8,7 +8,7 @@ import io.javalin.Javalin;
 @Service
 public class main {
     public static void main(String[] args) {
-        Javalin app = Javalin.create().start(8080);
+        Javalin app = Javalin.create().start(8090);
         
         app.get("/", ctx -> {
         ctx.result("Hello World from Java!");
